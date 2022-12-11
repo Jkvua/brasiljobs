b19f96ea012791bebc5eebd75d9e8b8c0fee0146
Jkvua <mila.e.ferreira28@gmail.com>
2022-12-11 16:43:36 -0300

    autenticação perfeita

--- src/views/SobreCadastro.vue
+++ src/views/SobreCadastro.vue
@@ -11,26 +11,28 @@
                         <v-card-text>
 
                             <v-form>
-                                <v-text-field label="Email" outlined></v-text-field>
-                                <v-text-field label="Nome Completo" outlined></v-text-field>
-                                <v-text-field label="Telefone" outlined></v-text-field>
-                                <v-text-field label="Cidade" outlined></v-text-field>
-                                <v-text-field label="CPF" outlined></v-text-field>
-                                <v-text-field id="password" name="password" label="Senha" type="password" outlined :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" @click:append="show1 = !show1"></v-text-field>
-
+                                <v-text-field label="Email" outlined v-model="user.email"></v-text-field>
+                                <v-text-field label="Nome Completo" outlined v-model="user.nome"></v-text-field>
+                                <v-text-field label="Ano de Nascimento (Ex = 2004)" type="date" outlined v-model="user.idade"></v-text-field>
+                                <v-text-field label="Telefone" outlined v-model="user.telefone"></v-text-field>
+                                <v-text-field label="Cidade" outlined v-model="user.cidade"></v-text-field>
+                                <v-text-field label="CPF" outlined v-model="user.cpf"></v-text-field>
+                                <v-text-field label="Senha" outlined :type="show ? 'text' : 'password'" :append-icon="show ? 'mdi-eye-outline' : 'mdi-eye-off-outline'" @click:append="show = !show" v-model="user.senha"></v-text-field>
                             </v-form>
                         </v-card-text>
                         <v-card-actions>
                             <v-spacer></v-spacer>
                             <v-row class="botoes">
                                 <v-col cols="3">
-                                    <v-btn color="#B2DFE1">Cadastrar-se</v-btn>
+                                    <v-btn color="#B2DFE1" @click="criarNovaConta">Cadastrar-se</v-btn>
                                 </v-col>
                                 <v-col cols="2"></v-col>
                                 <v-col cols="3">
                                     <v-btn color="#B2DFE1" @click="VoltarLogin">Cancelar</v-btn>
                                 </v-col>
                             </v-row>
+                            <v-snackbar transition="scale-transition" v-model="userExiste" dismissible outlined>Este email já está em uso.</v-snackbar>
+                            <v-snackbar transition="scale-transition" v-model="alertInvalidInfo" dismissible outlined>Preencha todos os campos.</v-snackbar>
                         </v-card-actions>
                     </v-card>
                 </v-flex>
@@ -41,10 +43,26 @@
 </template>
 
 <script>
+import * as fb from '@/plugins/firebase'
+
 export default {
-    name: 'SobreLogin',
-    props: {
-        source: String,
+    data() {
+        return {
+            show: false,
+            alertInvalidInfo: false,
+            invalidInfo: false,
+            userExiste: false,
+            user: {
+                email: '',
+                senha: '',
+                nome: '',
+                idade: '',
+                cpf: '',
+                cidade: '',
+                telefone: '',
+            },
+            
+        }
     },
 
     methods: {
@@ -52,9 +70,75 @@ export default {
             this.$router.push({
                 name: "Login"
             });
+
         },
-    }
-};
+        async criarNovaConta() {
+            try {
+                if (this.user.nome == null || this.user.nome == '' || this.user.idade == null || this.user.idade == '' || this.user.telefone == null || this.user.telefone == '' || this.user.cpf == null || this.user.cpf == '' || this.user.cidade == null || this.user.cidade == '' || this.user.email == null || this.user.email == '' || this.user.senha == null || this.user.senha == '') {
+                    this.invalidInfo = false
+                    this.alertInvalidInfo = true
+                } else {
+                    this.invalidInfo = true
+                }
+                if (this.invalidInfo == true) {
+                    await fb.auth.createUserWithEmailAndPassword(
+                        this.user.email,
+                        this.user.senha
+                    );
+                    this.registrarCadastro();
+                    this.entrar();
+                } else {
+                    this.alertInvalidInfo = true
+                }
+
+            } catch (error) {
+                const errorCode = error.code;
+                switch (errorCode) {
+                    case "auth/invalid-email":
+                        this.emailInvalid = true;
+                        this.user = {};
+                        break;
+                    case "auth/email-already-in-use":
+                        this.userExiste = true;
+                        this.user = {};
+                        break;
+                    default:
+                        this.userExiste = true;
+                        break;
+                }
+            }
+        },
+        async entrar() {
+            try {
+                await fb.auth.signInWithEmailAndPassword(
+                    this.user.email,
+                    this.user.senha,
+                );
+                this.$router.push({
+                    name: "Home"
+                });
+            } catch (error) {
+                this.erroEntrar = true;
+            }
+        },
+        async registrarCadastro() {
+            this.uid = fb.auth.currentUser.uid;
+            const res = await fb.profileCollection.add({
+                owner: this.uid,
+                Email: this.user.email,
+                Nome: this.user.nome,
+                Data_nasc: this.user.idade,
+                telefone: this.user.telefone,
+                cidade: this.user.cidade,
+                cpf: this.user.cpf,
+            });
+            const idCadastro = res.id
+            await fb.profileCollection.doc(idCadastro).update({
+                idCadastro: idCadastro
+            })
+        },
+    },
+}
 </script>
 
 <style scoped>
@@ -66,44 +150,6 @@ export default {
 .pont {
 
     text-align: center;
-    margin-left: 15rem;
+    margin-left: 10.5rem;
 }
 </style>
-
-
-
-
-
-<v-form>
-    <v-text-field
-      label="Nova tarefa"
-      v-model="novaTarefa"
-      @keydown.enter="adicionar"
-    >
-      <v-icon slot="append" @click="adicionar">mdi-send</v-icon>
-    </v-text-field>
-  </v-form>
-  <v-divider></v-divider>
-  <v-list>
-    <v-list-item-group>
-      <v-list-item v-for="tarefa of tarefas" :key="tarefa.titulo">
-        {{ tarefa.titulo }}
-      </v-list-item>
-    </v-list-item-group>
-  </v-list>
-
-novaTarefa: "",
-tarefas: [
-  { titulo: "Tarefa 1" },
-  { titulo: "Tarefa 2" },
-  { titulo: "Tarefa 3" },
-],
-};
-},
-methods: {
-adicionar(e) {
-e.preventDefault();
-this.tarefas.push({ titulo: this.novaTarefa });
-this.novaTarefa = "";
-},
-},
